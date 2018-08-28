8b2293c615f807e67c3d3be23e526d9ff9d6c51f
Mayck G <mtg.imports@gmail.com>
2018-08-28 09:20:36 -0300

    Nível de GMs - Alterado

--- src/common/Common.h
+++ src/common/Common.h
@@ -148,11 +148,15 @@ enum TimeConstants
 
 enum AccountTypes
 {
-    SEC_PLAYER         = 0,
-    SEC_MODERATOR      = 1,
-    SEC_GAMEMASTER     = 2,
-    SEC_ADMINISTRATOR  = 3,
-    SEC_CONSOLE        = 4                                  // must be always last in list, accounts must have less security level always also
+    SEC_PLAYER			= 0,
+	SEC_VIP				= 1,
+	SEC_DIVULGADOR		= 2,
+	SEC_SUPORTE			= 3,
+	SEC_EVENTOS			= 4,
+    SEC_MODERATOR		= 5,
+    SEC_GAMEMASTER		= 6,
+    SEC_ADMINISTRATOR	= 7,
+    SEC_CONSOLE			= 8                                 // must be always last in list, accounts must have less security level always also
 };
 
 enum LocaleConstant
